9e349e9623a6824e8cac9dbbf31edfd61cd70dc1
JonathanJoseGonzalez <110431669+JonathanJoseGonzalez@users.noreply.github.com>
2024-06-10 13:17:56 -0300

    Curso "rápido" de Bash Scripting do THM

---
+++ Scripts/FATEC/THM/scpt1.sh
@@ -0,0 +1,7 @@
+# um comentário em Bash é feito com a hashtag (#)
+# um script em Bash sempre começa com a seguinte linha de comando: #!/bin/bash
+# Isso serve para que o seu shell (qualquer um que esteja usando) saiba que precisa rodar o arquivo usando o bash no terminal (CMD)
+#Assume-se que o leitor já tenha um conhecimento prévio de Linux. Se não tiver, passe pelos Fundamentos de Linux de outro roadmap.
+#Não se incluirá o '#!/bin/bash' no começo dos scripts de aula, pois assume-se que todos os terão.
+#Para rodar o script devemos dar permissão para ele como o comando: chmod +x seuarquivo.sh ------ e o executamos com o './'; Exemplo: ./seuarquivo.sh
+
